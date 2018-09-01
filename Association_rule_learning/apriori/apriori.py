2507a6269dfe0c0a21f9bb083d050fb243ad4623
Ankur Gupta <32750675+ankur9352@users.noreply.github.com>
2018-09-01 12:18:05 +0530

    Update apriori.py

--- Association_rule_learning/apriori/apriori.py
+++ Association_rule_learning/apriori/apriori.py
@@ -1,1 +1,12 @@
 
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+data=pd.read_csv("Market_Basket_Optimisation.csv",header=None)
+transactions=[]
+for i in range(7501):
+	transactions.append([str(data.values[i,j]) for j in range(0,20)])
+from apyori import apriori
+rules=apriori(transactions,min_support=0.003,min_confidence=0.2,min_lift=3,min_length=2)
+res=list(rules)
+print(res)
